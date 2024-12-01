7562c53f02d635dce378a4dfeb767c774ad7cec7
karyn <tan.fe@northeastern.edu>
2024-12-01 00:52:13 -0500

    separated homepage view and controller

---
+++ NUSwap/Home Page/Home/HomePageTableViewManager.swift
@@ -0,0 +1,8 @@
+//
+//  HomePageTableViewManager.swift
+//  NUSwap
+//
+//  Created by Karyn T on 11/30/24.
+//
+
+import Foundation
---
+++ NUSwap/Home Page/Home/HomePageView.swift
@@ -0,0 +1,20 @@
+//
+//  HomePageView.swift
+//  NUSwap
+//
+//  Created by Karyn T on 11/30/24.
+//
+
+import UIKit
+
+class HomePageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
