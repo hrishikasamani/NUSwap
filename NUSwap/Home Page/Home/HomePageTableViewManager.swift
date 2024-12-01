1546596c93d299ec07ce8a00495e2def6e90d881
karyn <tan.fe@northeastern.edu>
2024-12-01 00:56:08 -0500

    separated homepage view and controller

--- NUSwap/Home Page/Home/HomePageTableViewManager.swift
+++ NUSwap/Home Page/Home/HomePageTableViewManager.swift
@@ -2,7 +2,29 @@
 //  HomePageTableViewManager.swift
 //  NUSwap
 //
-//  Created by Karyn T on 11/30/24.
+//  Created by Preksha Patil on 30/11/24.
 //
 
-import Foundation
+import UIKit
+
+extension HomePageViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "HomeCell", for: indexPath) as? HomePageTableViewCell else {
+            return UITableViewCell()
+        }
+        let item = items[indexPath.row]
+        cell.configure(with: item)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedItem = items[indexPath.row]
+        let itemDescriptionVC = ItemDescriptionViewController()
+        itemDescriptionVC.item = selectedItem
+        navigationController?.pushViewController(itemDescriptionVC, animated: true)
+    }
+}
--- NUSwap/Home Page/Home/HomePageView.swift
+++ NUSwap/Home Page/Home/HomePageView.swift
@@ -8,13 +8,59 @@
 import UIKit
 
 class HomePageView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var tableViewHome: UITableView!
+    var emptyListLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        
+        setupTableView()
+        setupEmptyListLabel()
+        
+        initConstraints()
     }
-    */
-
+    
+    func setupTableView() {
+        tableViewHome = UITableView()
+        tableViewHome.register(HomePageTableViewCell.self, forCellReuseIdentifier: "HomeCell")
+        tableViewHome.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(tableViewHome)
+    }
+    
+    func setupEmptyListLabel() {
+        emptyListLabel = UILabel()
+        emptyListLabel.text = """
+                              Welcome to the marketplace!
+                              
+                              No listings here yet.
+                              
+                              Please check back later.
+                              """
+        emptyListLabel.textColor = .gray
+        emptyListLabel.textAlignment = .center
+        emptyListLabel.font = UIFont.systemFont(ofSize: 20)
+        emptyListLabel.isHidden = true
+        emptyListLabel.numberOfLines = 0
+        emptyListLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(emptyListLabel)
+    }
+    
+    func initConstraints() {
+        NSLayoutConstraint.activate([
+            tableViewHome.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            tableViewHome.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+            tableViewHome.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
+            tableViewHome.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
+            
+            emptyListLabel.topAnchor.constraint(equalTo: self.tableViewHome.topAnchor, constant: 100),
+            emptyListLabel.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor),
+            emptyListLabel.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            emptyListLabel.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
